c5e38878b7fd68ec132caedbcde911427ef7a377
cw <ckddn2820@gmail.com>
2020-09-25 18:04:10 +0900

    vanilla JDBC

--- hello_spring/src/main/java/spring_study/hello_spring/SpringConfig.java
+++ hello_spring/src/main/java/spring_study/hello_spring/SpringConfig.java
@@ -1,16 +1,28 @@
 package spring_study.hello_spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import spring_study.hello_spring.repository.JdbcMemberRepository;
 import spring_study.hello_spring.repository.MemberRepository;
 import spring_study.hello_spring.repository.MemoryMemberRepository;
 import spring_study.hello_spring.service.MemberService;
+
+import javax.sql.DataSource;
 
 /**
  * java 코드로 직접 스프링 빈을 등록하는 방법
  */
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 
     @Bean
     public MemberService memberService(){
@@ -19,6 +31,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
