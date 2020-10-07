d3b247288fafc2ee6ed06786d3738c9ad0af922e
cw <ckddn2820@gmail.com>
2020-10-07 16:52:52 +0900

    integration test

--- hello_spring/src/test/java/spring_study/hello_spring/service/MemberServiceIntegrationTest.java
+++ hello_spring/src/test/java/spring_study/hello_spring/service/MemberServiceIntegrationTest.java
@@ -1,10 +1,11 @@
 package spring_study.hello_spring.service;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import spring_study.hello_spring.domain.Member;
-import spring_study.hello_spring.repository.MemoryMemberRepository;
+import spring_study.hello_spring.repository.MemberRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -13,23 +14,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * 테스트의 메소드 이름은 한글로 해도 괜찮다 (더 직관적)
  */
 
-class MemberServiceTest {
+@SpringBootTest
+@Transactional  // @Transactional 어노테이션의 기능: 테스트가 끝나면 ROLLBACK 하는 기능
+                // test case 에서만 ROLLBACK
+                // 개발 코드에서는 COMMIT 해줌 (커밋하고 싶으면 @COMMIT 을 써도 됨)
+class MemberServiceIntegrationTest {
 
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
-
-    // test 실행 전 이루어져야 할 것
-    @BeforeEach
-    public void beforeEach(){
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    // 각각의 test 이후에 이루어져야 할 것
-    @AfterEach
-    public void afterEach(){
-        memberRepository.clearStore();
-    }
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     void 회원가입() {
@@ -56,35 +48,13 @@ class MemberServiceTest {
         //when
         memberService.join(member1);
 
-        /**
-         * 정해진 Exception이 발생하는지를 검증하는 방법
-         */
+
+        // 정해진 Exception 이 발생하는지를 검증하는 방법
         assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
-        /**
-         * 메시지를 검증하는 방법
-         */
+        // 메시지를 검증하는 방법
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
-/*  try ~ catch 문을 사용해도 되지만 위의 간단한 코드 사용하는 것이 더 좋음
-        try{
-            memberService.join(member2);
-            fail();
-        } catch (IllegalStateException e){
-            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-        }
-*/
-
-        //then
-
-    }
-
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
     }
 }
